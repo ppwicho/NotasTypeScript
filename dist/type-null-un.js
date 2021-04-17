d56752218b35a78ebc68e8a302da8c11e5d112f3
@ppwicho <luis89@gmail.com>
2021-04-16 21:06:06 -0500

    modulos en typescript

--- dist/type-null-un.js
+++
@@ -1,7 +0,0 @@
-var nullVariable;
-nullVariable = null;
-//nullVariable=1; // error ! 
-var otherVariable = null;
-otherVariable = 'test';
-console.log('nullVariable', nullVariable);
-console.log('otheVariable', otherVariable);
